4c6f8af1102c124c4a8399b6d8147dd81d64be0d
Jani Rahkola <jani.rahkola@gmail.com>
2013-02-05 00:55:43 +0200

    draws arrows to ally towers and enemy heros

--- src/core.lua
+++ src/core.lua
@@ -1,3 +1,9 @@
+log = {}
+
+function log:info(msg)
+   Echo(msg)
+end
+
 local function randomHeroName()
    local heroNames = dofile("game/bots/mybot/hero_names.lua")
    local i = math.random(#heroNames)
@@ -7,10 +13,11 @@ end
 function object:onpickframe()
    if not self:HasSelectedHero() then
       local hero = randomHeroName()
-      Echo("Trying to pick "..hero)
+      log:info("Trying to pick "..hero)
+
       self:SelectHero(hero)
       if self:HasSelectedHero() then
-         Echo("Picked "..hero)
+         log:info("Picked "..hero)
          self:Ready()
       end
    end
@@ -20,6 +27,65 @@ function object:oncombatevent(event)
    
 end
 
-function object:onthink(gameVariables)
+local UNIT = 0x0000001
+local BUILDING = 0x0000002
+local HERO = 0x0000004
+local POWERUP = 0x0000008
+local GADGET = 0x0000010
+local ALIVE = 0x0000020
+local CORPSE = 0x0000040
 
+local function isAlly(botBrain, unit)
+   return unit:GetTeam() == botBrain:GetHeroUnit():GetTeam()
+end
+
+local function isEnemy(botBrain, unit)
+   return not isAlly(botBrain, unit)
+end
+
+local function allyTowersAlive(botBrain)
+   local buildings = HoN.GetUnitsInRadius(Vector3.Create(),
+                                          99999,
+                                          ALIVE + BUILDING)
+   local towers = {}
+   for _, building in pairs(buildings) do
+      if building:IsTower() and isAlly(botBrain, building) then
+         table.insert(towers, building)
+      end
+   end
+   
+   return towers
+end
+
+local function playerPosition(botBrain)
+   return botBrain:GetHeroUnit():GetPosition()
+end
+
+local function enemyHeros(botBrain)
+   local units = HoN.GetUnitsInRadius(Vector3.Create(), 99999, HERO + ALIVE)
+   local heros = {}
+   for _, unit in pairs(units) do
+      if isEnemy(botBrain, unit) then
+         table.insert(heros, unit)
+      end
+   end
+
+   return heros
+end
+
+local function sense(botBrain, gameVariables)
+   for _, tower in pairs(allyTowersAlive(botBrain)) do
+      HoN.DrawDebugLine(playerPosition(botBrain),
+                        tower:GetPosition(),
+                        true, "blue")
+   end
+   for _, enemy in pairs(enemyHeros(botBrain)) do
+      HoN.DrawDebugLine(playerPosition(botBrain),
+                        enemy:GetPosition(),
+                        true, "red")
+   end
+end
+
+function object:onthink(gameVariables)
+   sense(self, gameVariables)
 end
